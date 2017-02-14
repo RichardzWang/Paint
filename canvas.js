b7089c3ef83362cc3ede88061fd0fc74d35d1dba
nicolecheng <ncheng@stuy.edu>
2017-02-14 09:46:50 -0500

    a

--- canvas.js
+++ canvas.js
@@ -9,12 +9,28 @@ var getrekt = function(e){
     ctx.fillRect(x - 75/2,y - 100,75,100);
 };
 
-c.addEventListener("click", getrekt);
+// c.addEventListener("click", getrekt);
 
 button.addEventListener("click", function(){
-	var ctx = c.getContext('2d');
-	ctx.clearRect(0,0,c.clientWidth,c.clientHeight);	
+    var ctx = c.getContext('2d');
+    ctx.clearRect(0,0,c.clientWidth,c.clientHeight);	
 });
 
+var dots = function(e){
+    console.log("!!!!!!!!!!");
+    var x = e.clientX;
+    var y = e.clientY;
+    var ctx = c.getContext('2d');
+    ctx.fillStyle = "#FF0000";
+    ctx.arc(x,y,20,0,2*Math.PI);
+    ctx.fill();
+    ctx.beginPath();
+    moveTo(x,y);
+    ctx.lineTo(x,y);
+    ctx.stroke();
+    ctx.fill();
+};
 
+
+c.addEventListener("click", dots);
 
